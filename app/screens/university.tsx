e5fedddfc57bf8a676553434e029a454a84198dc
Jun Seulgi <us04185@gmail.com>
2023-05-17 01:08:52 +0900

    :sparkles: @jiggag/react-native-kakao-maps 세팅

--- app/screens/university.tsx
+++ app/screens/university.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import styled from '@emotion/native';
 import {Input} from '../components/common/input';
+import {KakaoMapView} from '@jiggag/react-native-kakao-maps';
 
 const getHeightRatio = (height: number) =>
   Math.floor((height / 812) * 100) + '%';
@@ -17,6 +18,27 @@ const UniversityScreen = () => {
           onChangeText={text => setUniversity(text)}
           placeholder="학교명을 검색하세요"
           autoFocus
+        />
+        <KakaoMapView
+          markerList={[
+            {
+              lat: 37.59523,
+              lng: 127.086,
+              markerName: 'marker',
+            },
+            {
+              lat: 37.59523,
+              lng: 127.08705,
+              markerName: 'marker2',
+            },
+          ]}
+          width={300}
+          height={500}
+          centerPoint={{
+            lat: 37.59523,
+            lng: 127.086,
+          }}
+          onChange={() => {}}
         />
       </Container>
     </SafeAreaView>
