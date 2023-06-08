5b910a6d091987ee35f76facf4fc7aed2f33376d
전슬기 <90638504+JUNSEULGI@users.noreply.github.com>
2023-06-08 19:18:36 +0900

    feat: naver map (#5)
    * :sparkles: Naver map build success in android
    
    * :bug: Fix ios build error from nmap
    
    * :green_heart: Pod install

--- app/screens/university.tsx
+++ app/screens/university.tsx
@@ -2,12 +2,22 @@ import React, {useState} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import styled from '@emotion/native';
 import {Input} from '../components/common/input';
+import NaverMapView, {
+  Circle,
+  Marker,
+  Path,
+  Polyline,
+  Polygon,
+} from 'react-native-nmap';
 
 const getHeightRatio = (height: number) =>
   Math.floor((height / 812) * 100) + '%';
 
 const UniversityScreen = () => {
   const [university, setUniversity] = useState('');
+  const P0 = {latitude: 37.564362, longitude: 126.977011};
+  const P1 = {latitude: 37.565051, longitude: 126.978567};
+  const P2 = {latitude: 37.565383, longitude: 126.976292};
   return (
     <SafeAreaView>
       <Container>
@@ -16,8 +26,46 @@ const UniversityScreen = () => {
           value={university}
           onChangeText={text => setUniversity(text)}
           placeholder="학교명을 검색하세요"
-          autoFocus
+          // autoFocus
         />
+        <NaverMapView
+          style={{width: '100%', height: '60%'}}
+          showsMyLocationButton={true}
+          center={{...P0, zoom: 16}}
+          onTouch={() => console.log('onTouch')}
+          onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
+          <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')} />
+          <Marker
+            coordinate={P1}
+            pinColor="blue"
+            onClick={() => console.warn('onClick! p1')}
+          />
+          <Marker
+            coordinate={P2}
+            pinColor="red"
+            onClick={() => console.warn('onClick! p2')}
+          />
+          <Path
+            coordinates={[P0, P1]}
+            onClick={() => console.warn('onClick! path')}
+            width={10}
+          />
+          <Polyline
+            coordinates={[P1, P2]}
+            onClick={() => console.warn('onClick! polyline')}
+          />
+          <Circle
+            coordinate={P0}
+            color={'rgba(255,0,0,0.3)'}
+            radius={200}
+            onClick={() => console.warn('onClick! circle')}
+          />
+          <Polygon
+            coordinates={[P0, P1, P2]}
+            color={`rgba(0, 0, 0, 0.5)`}
+            onClick={() => console.warn('onClick! polygon')}
+          />
+        </NaverMapView>
       </Container>
     </SafeAreaView>
   );
