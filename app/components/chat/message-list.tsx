0745b3b00a7dc9fdab607b98fd4d8fc7199fb1cc
Jun Seulgi <us04185@gmail.com>
2024-01-22 19:32:36 +0900

    :sparkles: ChatScreen scroll view -> flatlist

--- app/components/chat/message-list.tsx
+++ app/components/chat/message-list.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { FlatList } from 'react-native';
 import { Message } from 'components/chat/message';
 import { IMessage } from 'types/common';
 
 export const MessageList: React.FC<{ messages: IMessage[] }> = ({
   messages,
 }) => (
-  <ScrollView contentContainerStyle={{ padding: 20 }}>
-    {messages.map((msg, index, arr) => {
+  <FlatList
+    contentContainerStyle={{ padding: 20 }}
+    data={messages.map((msg, index, arr) => {
       const prev = arr[index - 1];
       const isSameSender =
         msg.user?._id === prev?.user?._id || (!msg.user && prev && !prev.user);
-      return (
-        <Message
-          key={msg._id}
-          data={msg}
-          isSending={!msg.user}
-          isSameSender={isSameSender}
-          isHost={msg.user?._id === 1}
-        />
-      );
+      return { ...msg, isSameSender, key: msg._id };
     })}
-  </ScrollView>
+    renderItem={(msg) => (
+      <Message
+        data={msg.item}
+        isSending={!msg.item.user}
+        isSameSender={msg.item.isSameSender}
+        isHost={msg.item.user?._id === 1}
+      />
+    )}
+  />
 );
