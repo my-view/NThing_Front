32496389c08b4426886efa3dddc2cb20a6a91edd
jungsuin <suin9207@ex-it.co.kr>
2024-05-23 21:03:10 +0900

    :sparkles: Item component navigation 수정

--- app/components/common/item.tsx
+++ app/components/common/item.tsx
@@ -4,24 +4,22 @@ import { Font15W500, Font16W600, Font12W400 } from 'components/common/text';
 import { formatPrice, convertToRelativeTime } from 'assets/util/format';
 import { PurchaseItemType } from 'types/common';
 import { HeartButton } from './heart-button';
-import { Image, View } from 'react-native';
+import { Image, View, Pressable, Alert } from 'react-native';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 import axios from 'axios';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { tradeQueryKeys } from '~/key/map';
-import { UsefetchMapTrade } from '~/hooks/map';
+import { getStorage } from '~/assets/util/storage';
+import { TOKEN_STORAGE_KEY } from '~/assets/util/constants';
+import { navigationRef } from '~/../RootNavigation';
 
 export const Item = ({
   data,
   index,
   listLength,
-  removeLikeKey,
   useHeartButton = true,
 }: {
   data: PurchaseItemType;
   index: number;
   listLength: number;
-  removeLikeKey?: any;
   useHeartButton?: boolean;
 }) => {
   const {
@@ -33,12 +31,8 @@ export const Item = ({
     denominator,
     liked,
     id,
-    latitude,
-    longitude,
-    status,
     image,
   } = data;
-  const queryClient = useQueryClient();
   const [lineCount, setLineCount] = useState(1);
 
   const singleLine = 16;
@@ -51,50 +45,60 @@ export const Item = ({
 
   return (
     <>
-      <Box>
-        <ItemBox>
-          <ItemImg source={{ uri: image }} />
+      <Pressable
+        onPress={async () => {
+          const token = await getStorage(TOKEN_STORAGE_KEY);
+          if (token)
+            return navigationRef.current.navigate('TradeScreen', {
+              id: id,
+            });
+          Alert.alert('로그인 후 이용해주세요!');
+          navigationRef.current.navigate('RootScreen');
+        }}
+      >
+        <Box>
+          <ItemBox>
+            <ItemImg source={{ uri: image }} />
+            <InfoBox>
+              <Title
+                style={{
+                  lineHeight: lineCount >= 1 ? singleLine : multiLine,
+                }}
+                numberOfLines={2}
+                onTextLayout={handleTextLayout}
+              >
+                {title}
+              </Title>
+              <TradeInfo>
+                {place} · {convertToRelativeTime(date)}
+              </TradeInfo>
+              <PricePersonBox>
+                <Price>{formatPrice(price)}원</Price>
+                <Person>
+                  <N>{numerator}</N>/{denominator}명
+                </Person>
+              </PricePersonBox>
+            </InfoBox>
+          </ItemBox>
 
-          <InfoBox>
-            <Title
-              style={{
-                lineHeight: lineCount >= 1 ? singleLine : multiLine,
-              }}
-              numberOfLines={2}
-              onTextLayout={handleTextLayout}
-            >
-              {title}
-            </Title>
-            <TradeInfo>
-              {place} · {convertToRelativeTime(date)}
-            </TradeInfo>
-            <PricePersonBox>
-              <Price>{formatPrice(price)}원</Price>
-              <Person>
-                <N>{numerator}</N>/{denominator}명
-              </Person>
-            </PricePersonBox>
-          </InfoBox>
-        </ItemBox>
-
-        {useHeartButton && (
-          <HeartButton
-            isLike={liked}
-            onClick={(isLiked) => {
-              console.log('좋아요', isLiked);
-              axios
-                .post(`/purchase/${id}/like`, {
+          {useHeartButton && (
+            <HeartButton
+              isLike={liked}
+              onClick={(isLiked) => {
+                console.log('좋아요', isLiked);
+                axios.post(`/purchase/${id}/like`, {
                   value: isLiked,
-                })
-                .then((res) => {
-                  // 여기서 좋아요 해제하면 맵에가면 좋아요 최신화해야하는데 키를 날릴까
-                  // 아니면 화면 잡히면 refetch를 해야하나..
-                  // queryClient.invalidateQueries(removeLikeKey);
                 });
-            }}
-          />
-        )}
-      </Box>
+                // .then((res) => {
+                //   // 여기서 좋아요 해제하면 맵에가면 좋아요 최신화해야하는데 키를 날릴까
+                //   // 아니면 화면 잡히면 refetch를 해야하나..
+                //   // queryClient.invalidateQueries(removeLikeKey);
+                // });
+              }}
+            />
+          )}
+        </Box>
+      </Pressable>
       <Divider
         style={{
           borderBottomWidth: index === listLength ? 0 : 1,
--- app/screens/participation-trade.tsx
+++ app/screens/participation-trade.tsx
@@ -12,17 +12,12 @@ const ParticipationTradeScreen = ({ navigation }: any) => {
 
   const renderItem = useCallback(
     (item: any, index: number) => (
-      <Pressable
-        key={index}
-        onPress={() => navigation.navigate('TradeScreen', { id: item.id })}
-      >
-        <Item
-          data={item}
-          index={index}
-          listLength={participationTradeList?.data?.length - 1}
-          useHeartButton={false}
-        />
-      </Pressable>
+      <Item
+        data={item}
+        index={index}
+        listLength={participationTradeList?.data?.length - 1}
+        useHeartButton={false}
+      />
     ),
     [],
   );
